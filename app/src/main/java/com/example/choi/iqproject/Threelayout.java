e20896a30c47203928ae22a936f126ccd1f0ad67
ruint03 <qlsbinu@yonsei.ac.kr>
2019-11-27 18:43:49 +0900

    listview clear

--- app/src/main/java/com/example/choi/iqproject/Threelayout.java
+++ app/src/main/java/com/example/choi/iqproject/Threelayout.java
@@ -1,5 +1,7 @@
 package com.example.choi.iqproject;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,13 +9,33 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class Threelayout extends Fragment {
     View v;
+    private Context mContext;
+    ListView listView_play;
+    ArrayList<ListViewItem_play> items;
+
+    public Threelayout() {}
+    @SuppressLint("ValidFragment")
+    public Threelayout(Context mContext) {
+        this.mContext = mContext;
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         v = inflater.inflate(R.layout.three_layout, container, false);
-return v;
+        listView_play = v.findViewById(R.id.list_play);
+        items = new ArrayList<>();
+        items.add(new ListViewItem_play("<1>", "감각행동 추구", "(Sensory seeking behaviors)"));
+        items.add(new ListViewItem_play("<2>", "감각 회피 행동","(Sensory avoiding behaviors)"));
+        listView_play.setAdapter(new ListViewAdapter_play(mContext, items));
+        return v;
     }
 }
